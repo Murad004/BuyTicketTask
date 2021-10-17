7d5042d4908e0c6ba8bb6a72029cb74b9ece08ae
Sadi_er59 <Sadi_er59@STHQ0123-07>
2021-10-17 18:45:58 +0400

    Add project files.

---
+++ BuyTicketTask/Domain/Abstraction/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyTicketTask.Domain.Abstraction
+{
+    public interface IRepository<T>
+    {
+        T GetAll(int Id);
+        void AddData(T data);
+        void UpdateData(T data);
+        void DeleteData(T data);
+    }
+}
